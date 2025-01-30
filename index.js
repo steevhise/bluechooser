af2e98ee1361e756d31de8459dc97687efba2b8b
Steev Hise <steev@datamassage.com>
2025-01-29 21:33:00 -0700

    perfecting the web side.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const {blueScan} = require('./blueScan');
+const {blueScan, sleep} = require('./blueScan');
+const timeout = 5;  // in seconds
 
 // Define routes and middleware here
 // we basically just want 2 routes:
@@ -11,14 +12,15 @@ const {blueScan} = require('./blueScan');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 // for timing out the requests, but the problem is if bluetooth is busy then it complains.
-app.use((req, res, next) => {
-    res.setTimeout(5000, ()=>{
+// we dont need this as we're oding the timeout ourselves
+/*app.use((req, res, next) => {
+    res.setTimeout(9000, ()=>{
         console.log('Request has timed out.');
         res.sendStatus(408);
-        res.end();
+        // res.end();
+        return;
     });
-    next();
-});
+});*/
 
 router.route('/')
     .get(async function (req, res) {
@@ -28,11 +30,15 @@ router.route('/')
         res.end();
     })
     .post(async function (req,res) {
-        console.log('POST');
         const data = req.body;
-        console.log(data);
-        await blueScan(data.choice);
-        res.locals.message = `Connection to ${data.choice} Complete!`;  // TODO: or catch error/timeout?
+        console.log('POST request', data.choice);
+        let r = await Promise.race([sleep(timeout * 1000), blueScan(data.choice)])
+        console.log(r);
+        if (r === 'times up!') {
+            res.locals.message = `Connection to ${data.choice} has timed out!`;
+        } else {
+            res.locals.message = `Connection to ${data.choice} Complete!`;  // TODO: or catch error/timeout?
+        }
         res.render('results');
         res.end();
     });
