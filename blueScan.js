ab3f7390f0f631c59e591182fb7e65f0f38961d5
steev <you@example.com>
2025-04-30 19:37:18 -0700

    finished getting it all to work with 2 speakers. yay.

--- blueScan.js
+++ blueScan.js
@@ -3,9 +3,12 @@ const {bluetooth} = createBluetooth()
 const Debug = require('debug')('bluetooth');
 // const defaultTarget = 'Membrillo';
 
-const blueScan = async (target = '') => {
+const blueScan = async (target = '', target2 = '') => {
 
-    Debug('scanning for %s...', target)
+    Debug('scanning for %s...%s', target + ' ' + target2)
+    if(target === target2) {
+	    target2 = '';
+    }
     const deviceInfo = [];
     const adapter = await bluetooth.defaultAdapter()
 
@@ -14,6 +17,8 @@ const blueScan = async (target = '') => {
 
     // otherwise, we ARE discovering...
     const devices = await adapter.devices();
+    Debug(devices);
+
     let device= {};
 
     for (const d of devices) {
@@ -21,34 +26,62 @@ const blueScan = async (target = '') => {
         const name = await device.getAlias();
         const paired = await device.isPaired();
         const connected = await device.isConnected();
-        Debug(d,name);
-        deviceInfo.push({
-            id: d,
-            name,
-            paired,
-            connected
-        });
+        Debug(d,name,paired);
+
+	if(!target && !target2) {
+           // we're just scanning, not connecting.
+	   Debug('just scanning, not connecting');   
+	}
 
         if((target) && (name === target)) {
             try {
-                if (!await device.isPaired())
+                if (!await device.isPaired()) {
                     await device.pair();
-                Debug('paired with %s', target);
-                if (!await device.isConnected())
+		    paired = true;
+                    Debug('paired with %s', name);
+		}
+                if (!await device.isConnected()) {
                     await device.connect();   // TODO: catch error?
-                    Debug('connected to %s', name);
+                    Debug('connected to speaker 1: %s', name);
+		    connected = true;
+		} else {
+                    Debug('already connected to speaker 1 "%s"', name);
+		}
             } catch(e) {
-                console.log('something wrong with BT connect', e.text);
+                console.log('something wrong connecting with first speaker ' + target, e.text);
                 return 'timeout';
             }
-
-            break;
         }
+	else if((target2) && (name === target2)) {
+            try {
+                if (!await device.isPaired()) {
+                    await device.pair();
+		    paired = true;
+                    Debug('paired with %s', name);
+	        }
+                if (!await device.isConnected()) {
+                    await device.connect();   // TODO: catch error?
+                    Debug('connected to speaker 2: %s', name);
+		    connected = true;
+		} else {
+                    Debug('already connected to speaker 2 "%s"', name);
+		}
+            } catch(e) {
+                console.log('something wrong connecting with second speaker ' + target2, e.text);
+                return 'timeout';
+            }
+	}
+
+	deviceInfo.push({
+              id: d,
+              name,
+              paired,
+              connected
+	});
     }
 
-    // Debug('devices: %o', deviceInfo);
+    Debug('devices: %o', deviceInfo);
     await adapter.stopDiscovery()
-    //console.log('dev', device.device);
     return deviceInfo;
 }
 
